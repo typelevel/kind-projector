646c2bd2d38fda938e95f9170b827963f02a7e13
Sam Halliday <sam.halliday@gmail.com>
2017-04-06 19:44:57 +0100

    add pcplod

---
+++ src/test/resources/moo.scala
@@ -0,0 +1,4 @@
+object Moo {
+  def test[F[_]](): Unit = {}
+  t@test@est[E@either@ither[Thr@throwable@owable, ?]]()
+}
---
+++ src/test/scala/pc.scala
@@ -0,0 +1,20 @@
+package pc
+
+import org.junit._
+import org.ensime.pcplod._
+
+class PresentationCompilerTest {
+  import Assert._
+
+  @Test
+  def notReportErrorsInValidCode(): Unit = withMrPlod("moo.scala") { mr: MrPlod =>
+    assertTrue(mr.messages.toString, mr.messages.size == 0)
+
+    assertEquals(Some("[F[_]]()Unit"), mr.typeAtPoint('test))
+
+    // failing to return type information here
+    //assertNotEquals(Some("<notype>"), mr.typeAtPoint('either))
+    //assertNotEquals(Some("<notype>"), mr.typeAtPoint('throwable))
+  }
+
+}
