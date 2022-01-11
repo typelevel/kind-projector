ab8f0b8c42e6449324f7868e8d5f53bdcbc907c7
Ross A. Baker <ross@rossabaker.com>
2022-01-11 11:02:49 -0500

    Make it simpler to backpublish

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Provision a dev environment";
+
+  inputs = {
+    typelevel-nix.url = "github:rossabaker/typelevel-nix";
+    nixpkgs.follows = "typelevel-nix/nixpkgs";
+    flake-utils.follows = "typelevel-nix/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ typelevel-nix.overlay ];
+        };
+      in
+      {
+        devShell = pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevelShell ];
+          name = "kind-projector";
+          typelevelShell = {
+            jdk.package = pkgs.jdk8;
+          };
+          commands = [{
+            name = "back-publish";
+            help = "back publishes a tag to Sonatype with a specific Scala version";
+            command = "${./scripts/back-publish} $@";
+          }];
+        };
+      }
+    );
+}
